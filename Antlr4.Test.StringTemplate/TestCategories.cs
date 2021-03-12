53fcb51a325b1ba3d87848f9634503fa63662d48
Ken Domino <kenneth.domino@domemtech.com>
2021-03-11 19:56:53 -0500

    Update to build from git clone.

--- Antlr4.Test.StringTemplate/TestCategories.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Antlr4.Test.StringTemplate
-{
-    internal static class TestCategories
-    {
-        public const string ST4 = "ST4";
-
-        public const string Visualizer = "Visualizer";
-    }
-}
