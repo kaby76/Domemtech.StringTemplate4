72b05ddfed9d1daba63572cd1c3255fe55841c14
Ken Domino <kenneth.domino@domemtech.com>
2021-07-01 08:49:55 -0400

    Adding in a more complicated example.

--- Tester/Driver.cs
+++ Tester/Driver.cs
@@ -33,8 +33,22 @@ decl(name, value) ::= ""<name> = <value>;""
             {
                 var tree = Parsing.Parse("1+2") as ArithmeticParser.File_Context;
                 var tg = new TemplateGroupString(@"
-ex(c) ::= ""<if(c.PLUS)><first(c.expression).Text> PLUS <first(rest(c.expression)).Text><endif>""
+ex(c) ::= 
+""<if(c.PLUS)><first(c.expression).Text> PLUS <first(rest(c.expression)).Text><endif>""
 ");
+                tg.Verbose = true;
+                var template = tg.GetInstanceOf("ex");
+                template.Add("c", tree.expression()[0]);
+                var res = template.Render();
+                Console.WriteLine(res);
+            }
+            {
+                var tree = Parsing.Parse("1+2") as ArithmeticParser.File_Context;
+                var tg = new TemplateGroupString(@"
+ex(c) ::= ""<if(c.atom)><at(c.atom)><elseif(c.PLUS)><ex(first(c.expression))> PLUS <ex(first(rest(c.expression)))><elseif(c.MINUS)><ex(first(c.expression))> MINUS <ex(first(rest(c.expression)))><elseif(c.TIMES)><ex(first(c.expression))> TIMES <ex(first(rest(c.expression)))><elseif(c.DIV)><ex(first(c.expression))> DIV <ex(first(rest(c.expression)))><elseif(c.POW)><ex(first(c.expression))> POW <ex(first(rest(c.expression)))><elseif(c.LPAREN)><ex(first(c.expression))><endif>""
+at(c) ::= ""<c.Text>""
+");
+                tg.Verbose = true;
                 var template = tg.GetInstanceOf("ex");
                 template.Add("c", tree.expression()[0]);
                 var res = template.Render();
